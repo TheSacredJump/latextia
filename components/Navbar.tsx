ccfe3421bce1f3da9f94cc42571f4e31859b301c
SathyaCrowdVolt <sathya@crowdvolt.com>
2025-02-18 18:26:22 -0700

    Latextia Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs"
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-4 left-1/2 -translate-x-1/2 flex justify-between items-center p-4 w-full max-w-7xl mx-auto rounded-full border bg-white/50 backdrop-blur-sm z-50'>
+    <nav className='fixed top-16 left-1/2 -translate-x-1/2 flex justify-between items-center p-4 w-full max-w-7xl mx-auto rounded-full border bg-white/50 backdrop-blur-sm z-50'>
         <Link href="/" className='flex items-center gap-2'>
             <div className='w-8 h-8 bg-gradient-to-br from-green-500 to-emerald-500 rounded-lg'></div>
             <h1 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-emerald-600'>Latextia</h1>
---
+++ components/Update.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Update = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-10 w-full bg-gradient-to-r from-sky-400 via-violet-400 to-pink-400'>
+        <h1 className='text-white text-xl font-bold'>Latest Updates: Latextia has hit 10k overall users!</h1>
+    </div>
+  )
+}
+
+export default Update
